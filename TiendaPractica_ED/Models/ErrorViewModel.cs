ff6454607c7f4dc5df0834d29fe6c1df847cf31e
edinson rosales <edidy9v@gmail.com>
2023-09-29 12:36:13 -0500

    creacion del projec tienda ED

---
+++ TiendaPractica_ED/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiendaPractica_ED.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
